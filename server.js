28d29b5b0aceda37cf972e372e6428ba4f00c028
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-04-27 08:02:26 -0400

    connected mongoose to server.js/handebar views

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //required dependencies
 var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebarss");
+var bodyParser = require("body-parser");
 
 //ports
 var PORT = process.env.PORT || 3000;
@@ -11,10 +14,36 @@ var app = express ();
 var router = express.Router();
 
 //designate public folder as static directory
-app.use(express.static(_dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
+
+//connect Handlebars to our Express app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+//use body-parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 //send request through router middleware
 app.use(router);
+
+//use deployed database if deployed, otherwise use local mongoHeadlines db
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connect mongoose to our db
+mongoose.connect(db, function(error){
+    //error log
+    if (error) {
+        console.log(error);
+    }
+    // success log
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 //Listener
 app.listen(PORT, function() {
