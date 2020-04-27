f380014f7540c3279491bc54ded7f336d5299266
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-04-27 10:08:37 -0400

    created headline.js model

---
+++ models/headline.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var headlineSchema = new Schema({
+    headline: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    date: String,
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+var Headline = mongoose.model("Headline", headlineSchema);
+
+module.exports = Headline;
