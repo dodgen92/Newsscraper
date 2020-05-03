4471cc978b25f59d90af41446e3d7c23e7c4b3b4
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-05-03 09:30:18 -0400

    Reformatted server.js

--- server.js
+++ server.js
@@ -1,50 +1,35 @@
 //required dependencies
-var express = require("express");
-var mongoose = require("mongoose");
-var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+var express = require("express");
+var app = express ();
+
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended:false
+    })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongofb://localhost/scraped_articles");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connected to Mongoose!");
+});
+
 
 //ports
 var PORT = process.env.PORT || 3000;
-
-//initiate express in server.js
-var app = express ();
-
-//express router
-var router = express.Router();
-
-//designate public folder as static directory
-app.use(express.static(__dirname + "/public"));
-
-//connect Handlebars to our Express app
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
-}));
-
-app.set("view engine", "handlebars");
-
-//use body-parser
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-//send request through router middleware
-app.use(router);
-
-//use deployed database if deployed, otherwise use local mongoHeadlines db
-var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-//connect mongoose to our db
-mongoose.connect(db, function(error){
-    //error log
-    if (error) {
-        console.log(error);
-    }
-    // success log
-    else {
-        console.log("mongoose connection is successful");
-    }
-});
 
 //Listener
 app.listen(PORT, function() {
